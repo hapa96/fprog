377de4e1eb06053e774d86975fcf80fb3f2f2c67
Pascal Hauser <pascal.hauser457@gmail.com>
2020-10-04 14:13:43 +0200

    Add Assignment 3

--- 03_Lists/Assignment/PicturesASCII.hs
+++ 03_Lists/Assignment/PicturesASCII.hs
@@ -30,35 +30,40 @@ printPicture p = putStr (unlines p)
 
 -- a) Mirrors the given picture horizontally.
 flipH :: Picture -> Picture
-flipH = undefined
+flipH p = reverse p
 
 -- b) Mirrors the given picture vertically.
 -- hint = "Wrqr Mrvyr zhff frcneng hztrxrueg jreqra."
 flipV :: Picture -> Picture
-flipV = undefined
+flipV p = map reverse p
 
 -- c) Takes two pictures and puts the first above the second.
 -- hint = "Qnf fbyygra Fvr nhpu buar Uvysr uvaxevrtra."
 above :: Picture -> Picture -> Picture
-above = undefined
+above p_top p_bottom = p_top ++ p_bottom
 
 -- d) Takes two pictures and puts the first left of the second.
 -- hint = "Irejraqra Fvr qvr Shaxgvba mvcJvgu hz wr mjrv Mrvyra mh xbaxngravrera."
 beside :: Picture -> Picture -> Picture
-beside = undefined
-
+beside p_left p_right = zipWith (++) p_left p_right
 --------------------------------------------------------------------------------
 -- 2) Functions on Lists
 -- hint = "\65533oreyrtra Fvr fvpu mhrefg qvr Fvtanghe"
 --------------------------------------------------------------------------------
 -- a) Append an element at the end.
+add_elem :: Picture -> String -> Picture
+add_elem p e = p ++ [e]
 
 
 -- b) Reverse all but the first and the last element.
-
+rev :: Picture -> Picture
+rev p = head p : reverse ( tail (init p)) ++ [last p]
+-- Mit Pattern Match
+-- rev (a:as) = a: reverse (init as) ++ [last as]
 
 -- c) Insert an element at a given position.
-
+ins :: a -> Int -> [a] -> [a]
+ins a n as = take n as ++ a : drop n as
 
 
 
