e9c14bf3b746de6a863abd181d641c935985eee2
Pascal Hauser <pascal.hauser457@gmail.com>
2020-10-13 09:52:16 +0200

    Courseware 05

---
+++ 05_FunctionsII/Lecture/WS_LambdaExpressions.hs
@@ -0,0 +1,13 @@
+incAll :: [Int] -> [Int]
+incAll = todo
+
+addToAll :: Int -> [Int] -> [Int]
+addToAll = todo
+
+keepOld :: [Int] -> [Int]
+keepOld = todo
+
+dropShort :: [String] -> [String]
+dropShort = todo
+
+todo = error "TODO"
